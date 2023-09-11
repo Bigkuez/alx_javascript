20fb6eea48355e79db98230a99f48729badbc24b
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-11 23:49:46 +0100

    commit

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,16 +1,8 @@
-// Define an object or use the global 'window' object
-const function = {};
-
 // Define the 'add' function
 function add(a, b) {
     return a + b;
   }
-
-// Export the object (if you are using Node.js or a module system)
-// module.exports = myFunctions;
-
-// Or if you are in a browser environment, the function is already visible
-
-// Example usage:
-const result = function.add(5, 7);
-console.log(result); // Output: 12
+  
+  // Export the 'add' function
+  module.exports.add = add;
+  
