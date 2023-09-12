de1721817720a29994c107190384e2ebb1b22898
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-12 01:03:23 +0100

    commit

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,19 +1,33 @@
 #!/usr/bin/node
+
+// Define the initial object
 const myObject = {
     type: 'object',
     value: 12
   };
-  console.log(myObject);
-  /*
-  YOUR CODE HERE
-  */
-  myObject.incr = function () {
+  
+  // Define the incr function as a named function expression
+  function incr() {
     this.value++;
-  };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+  }
+  
+  // Display the initial object
+  console.log({ type: myObject.type, value: myObject.value });
+  
+  // Add the incr function to the object
+  myObject.incr = incr;
+  
+  // Define a helper function to hide the function in the output
+  function hideFunctionInOutput(key, value) {
+    if (typeof value === 'function') {
+      return '[Function]';
+    }
+    return value;
+  }
+  
+  // Increment and display the object three times with the function hidden
+  for (let i = 0; i < 3; i++) {
+    myObject.incr();
+    console.log(JSON.stringify(myObject, hideFunctionInOutput, 2));
+  }
   
