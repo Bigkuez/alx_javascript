86c477af88a10eb149737e878099e33dd1861c60
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-13 01:14:31 +0100

    commit

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,16 +1,36 @@
 const request = require('request');
 
-// Check if a movie ID is provided as a command-line argument
+// Mapping of episode numbers to movie IDs
+const episodeToMovieId = {
+  1: 1,
+  2: 5,
+  3: 6,
+  4: 4,
+  5: 2,
+  6: 3,
+  7: 7,
+};
+
+// Check if an episode number is provided as a command-line argument
 if (process.argv.length !== 3) {
   console.error('Usage: node starwars_title.js <episode_number>');
   process.exit(1);
 }
 
 // Get the episode number from the command-line arguments
-const episodeNumber = process.argv[2];
+const episodeNumber = parseInt(process.argv[2]);
+
+// Check if the provided episode number exists in the mapping
+if (!episodeToMovieId.hasOwnProperty(episodeNumber)) {
+  console.error(`Error: Movie with Episode Number ${episodeNumber} not found.`);
+  process.exit(1);
+}
+
+// Get the corresponding movie ID from the mapping
+const movieId = episodeToMovieId[episodeNumber];
 
 // Construct the API URL
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${episodeNumber}`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 // Make a GET request to the API
 request(apiUrl, (error, response, body) => {
@@ -28,12 +48,8 @@ request(apiUrl, (error, response, body) => {
     // Parse the JSON response
     const movieData = JSON.parse(body);
 
-    // Check if the episode_id matches the provided episode number
-    if (movieData && movieData.episode_id == episodeNumber) {
-      console.log(movieData.title);
-    } else {
-      console.error(`Error: Movie with Episode Number ${episodeNumber} not found.`);
-    }
+    // Print the title of the movie
+    console.log(movieData.title);
   } catch (parseError) {
     console.error('Error: Unable to parse response.');
     process.exit(1);
