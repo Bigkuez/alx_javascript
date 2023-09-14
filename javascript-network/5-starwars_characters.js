4d60c3219fabef1170ab3e7b3562df3a8a737bd3
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-14 02:19:35 +0100

    commit

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,15 +1,23 @@
 
 const request = require('request');
 
+// Get the movie ID from the command line arguments
 const movieId = process.argv[2];
+
+// Construct the URL for the specific movie
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
+// Make an HTTP request to fetch movie data
 request(url, function (_error, _response, body) {
+    // Parse the JSON response to extract character URLs
     const charactersUrl = JSON.parse(body)['characters'];
 
+    // Iterate through character URLs
     charactersUrl.forEach(characterUrl => {
+        // Make an HTTP request to fetch character data
         request(characterUrl, function (_error, _response, body) {
+            // Parse the JSON response to extract and print the character's name
             console.log(JSON.parse(body)['name']);
-        })
-    })
-})
+        });
+    });
+});
