f6da81b0ba9e4e2467804f4b6c232aa17c0a0eaf
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-09-27 14:57:41 +0100

    commit

--- Cookies_local_storage/src/3-index.js
+++ Cookies_local_storage/src/3-index.js
@@ -1,94 +1,78 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const logButton = document.getElementById("log");
-    const showCookiesButton = document.getElementById("btn-show-cook");
-  
-    logButton.addEventListener("click", setCookies);
-    showCookiesButton.addEventListener("click", showCookies);
-  
-    showWelcomeMessageOrForm();
-  });
-  
-  function setCookies() {
-    const firstnameValue = document.getElementById("firstname").value;
-    const emailValue = document.getElementById("email").value;
-  
-    // Calculate the expiration date 10 days from now
-    const expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + 10);
-  
-    // Set cookies with 10-day expiration
-    document.cookie = `firstname=${firstnameValue}; expires=${expirationDate.toUTCString()}; path=/`;
-    document.cookie = `email=${emailValue}; expires=${expirationDate.toUTCString()}; path=/`;
-  
-    alert("Cookies set successfully!");
-  }
-  
-  function showCookies() {
-    const firstname = getCookie("firstname");
-    const email = getCookie("email");
-  
-    const paragraph = document.createElement("p");
-    paragraph.innerHTML = `Email: ${email} - Firstname: ${firstname}`;
-    document.body.appendChild(paragraph);
-  }
-  
-  function getCookie(name) {
-    const cookies = document.cookie.split("; ");
-    for (const cookie of cookies) {
-      const [cookieName, cookieValue] = cookie.split("=");
-      if (cookieName === name) {
-        return cookieValue;
-      }
-    }
-    return "";
-  }
-  
-  function showForm() {
+    const myForm = document.getElementById("myForm");
     const welcomeMessage = document.getElementById("welcomeMessage");
-    if (welcomeMessage) {
-      welcomeMessage.style.display = "none";
-    }
-  
     const loginForm = document.getElementById("loginForm");
-    if (loginForm) {
-      loginForm.style.display = "block";
+
+    // Function to set cookies with a 10-day expiration
+    function setCookies() {
+        const firstnameValue = document.getElementById("firstname").value;
+        const emailValue = document.getElementById("email").value;
+
+        const now = new Date();
+        const expirationDate = new Date(now.getTime() + 10 * 24 * 60 * 60 * 1000); // 10 days
+        const expires = `expires=${expirationDate.toUTCString()}`;
+
+        document.cookie = `firstname=${firstnameValue}; ${expires}; path=/`;
+        document.cookie = `email=${emailValue}; ${expires}; path=/`;
+
+        alert("Cookies set successfully!");
     }
-  }
-  
-  function hideForm() {
-    const loginForm = document.getElementById("loginForm");
-    if (loginForm) {
-      loginForm.style.display = "none";
+
+    // Function to retrieve the value of a cookie by name
+    function getCookie(name) {
+        const cookies = document.cookie.split("; ");
+        for (const cookie of cookies) {
+            const [cookieName, cookieValue] = cookie.split("=");
+            if (cookieName === name) {
+                return decodeURIComponent(cookieValue);
+            }
+        }
+        return "";
     }
-  }
-  
-  function deleteCookiesAndShowForm() {
-    document.cookie = "firstname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-  
-    showForm();
-  }
-  
-  function showWelcomeMessageOrForm() {
-    const firstname = getCookie("firstname");
-    if (firstname) {
-      const welcomeMessage = document.createElement("h1");
-      welcomeMessage.id = "welcomeMessage";
-      welcomeMessage.innerHTML = `Welcome ${firstname} <a href="#" id="logout">(logout)</a>`;
-  
-      const logoutLink = document.getElementById("logout");
-      if (logoutLink) {
-        logoutLink.style.cssText = "font-weight: normal; font-style: italic; margin-left: 10px;";
-  
-        logoutLink.addEventListener("click", function () {
-          deleteCookiesAndShowForm();
-          welcomeMessage.style.display = "none";
-        });
-      }
-  
-      document.body.replaceWith(welcomeMessage);
-    } else {
-      showForm();
+
+    // Function to display the cookies
+    function showCookies() {
+        const firstnameCookie = getCookie("firstname");
+        const emailCookie = getCookie("email");
+
+        const paragraph = document.createElement("p");
+        paragraph.textContent = `Cookies: Email: ${emailCookie} - Firstname: ${firstnameCookie}`;
+        welcomeMessage.appendChild(paragraph);
     }
-  }
-  
+
+    // Function to delete cookies and show the form
+    function deleteCookiesAndShowForm() {
+        document.cookie = "firstname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        welcomeMessage.innerHTML = "";
+        loginForm.style.display = "block";
+    }
+
+    // Function to show the welcome message or form
+    function showWelcomeMessageOrForm() {
+        const firstnameCookie = getCookie("firstname");
+
+        if (firstnameCookie) {
+            // User is logged in
+            welcomeMessage.innerHTML = `<h1>Welcome ${firstnameCookie} <span id="logout">(logout)</span></h1>`;
+            const logoutLink = document.getElementById("logout");
+            logoutLink.style.cssText = "font-weight: normal; font-style: italic; position: relative; left: 10px; cursor: pointer;";
+            logoutLink.addEventListener("click", deleteCookiesAndShowForm);
+            loginForm.style.display = "none";
+        } else {
+            // User is not logged in
+            loginForm.style.display = "block";
+        }
+    }
+
+    // Event listener for the "Log me in" button
+    const loginButton = document.getElementById("loginButton");
+    loginButton.addEventListener("click", setCookies);
+
+    // Event listener for the "Show the cookies" button
+    const showCookiesButton = document.getElementById("showCookiesButton");
+    showCookiesButton.addEventListener("click", showCookies);
+
+    // Initial page load
+    showWelcomeMessageOrForm();
+});
